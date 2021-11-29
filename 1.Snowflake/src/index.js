f0c349d1078e99bd8547031e850c6326a1b8bc9f
liquiddevelopmentnet <info@minemix.de>
2021-11-29 19:38:56 +0100

    [1] Correcting example

--- 1.Snowflake/src/index.js
+++ 1.Snowflake/src/index.js
@@ -4,6 +4,5 @@ import { Snowflake } from "./Modules/Snowflake.js"
 
 /* Playground (uncomment for testing) */
 
-const mySnowflake = new Snowflake(914938362203353138n) // <- Your Snowflake ID
-console.log(mySnowflake.analyze().timestamp_unix)
-
+// const mySnowflake = new Snowflake(000000000000000000) // <- Your Snowflake ID
+// mySnowflake.analyze().timestamp_unix
